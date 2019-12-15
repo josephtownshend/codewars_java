6db15166b8207bbb016ecadcae329a04498f0a5b
joseph townshend <joetownshend@mac.com>
2019-12-15 14:14:59 +0000

    remove-string-space-8kyu

---
+++ src/codewars_kata/RemoveStringSpaces.java
@@ -0,0 +1,9 @@
+package codewars_kata;
+
+public class RemoveStringSpaces {
+	static String noSpace(final String x) {
+		String noWhiteSpace = x.replace(" ","");        
+	    return noWhiteSpace;
+	}
+}
+
